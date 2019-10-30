af0abfd3fa0c9c298e8a241a925541cdf69ba03f
Stefan Björnson <43110881+StefanBearson@users.noreply.github.com>
2019-10-30 20:55:00 +0100

    fix key problem

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,24 +8,29 @@ import {
   MDBCol,
   MDBCardFooter
 } from "mdbreact";
-import { number } from "prop-types";
+// import { number } from "prop-types";
 
 export default props => {
-  console.log(props);
-
   const phoneNumbersArray = props.result.phoneNumbers.split(",");
-  console.log(phoneNumbersArray);
+
   return (
-    <MDBCol lg='4' md='6'>
-      <MDBCard style={{ margin: "2px" }}>
+    <MDBCol
+      lg='4'
+      md='6'
+      style={{ padding: 0 }}
+      key={Math.random() * 10000000000}
+    >
+      <MDBCard style={{ margin: "8px" }}>
         <MDBCardImage
+          key={Math.random() * 10000000000}
           className='img-fluid'
-          style={{ width: "20%", marginLeft: "40%" }}
+          style={{ width: "20%", marginLeft: "40%", padding: "8px 0" }}
           src='http://blog.springfield.k12.or.us/yolanda/files/2009/02/person-placeholder-7.png'
           waves
         />
-        <MDBCardBody style={{ padding: 0 }}>
+        <MDBCardBody style={{ padding: 0 }} key={Math.random() * 10000000000}>
           <MDBCardTitle
+            key={Math.random() * 10000000000}
             style={{
               color: "white",
               margin: "0",
@@ -38,6 +43,7 @@ export default props => {
               : props.result.firstName + " " + props.result.lastName}
           </MDBCardTitle>
           <MDBCardText
+            key={Math.random() * 10000000000}
             style={{
               color: "white",
               margin: 0,
@@ -47,15 +53,20 @@ export default props => {
           >
             {props.result.department}
           </MDBCardText>
-          <MDBCardText style={{ paddingLeft: "8px" }}>
+          <MDBCardText
+            key={Math.random() * 10000000000}
+            style={{ paddingLeft: "8px" }}
+          >
             E-Mail: {props.result.eMail} <br />
             {/* Phone: {props.result.phoneNumbers} <br /> */}
             {phoneNumbersArray.map(number => (
-              <p style={{ margin: 0 }}>Phone: {number}</p>
+              <p key={Math.random() * 10000000000} style={{ margin: 0 }}>
+                Phone: {number}
+              </p>
             ))}
             Department: {props.result.department}
           </MDBCardText>
-          <MDBCardFooter>
+          <MDBCardFooter key={Math.random() * 10000000000}>
             Shortnumber (Skype):{" "}
             <a style={{ color: "blue" }}>{props.result.shortNumber}</a>
           </MDBCardFooter>
